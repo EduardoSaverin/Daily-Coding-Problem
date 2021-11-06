bd0f431d9c1f8f7649039beed1efbb0b3f4cc649
Sumiit Chaudhary <digitalcodecorporation@gmail.com>
2021-11-06 17:53:30 +0530

    Currency Conversion Problem

---
+++ dailycodingproblems/#32.py
@@ -0,0 +1,17 @@
+"""
+Suppose you are given a table of currency exchange rates, represented as a 2D array. 
+Determine whether there is a possible arbitrage: that is, whether there is some sequence of trades you can make, 
+starting with some amount A of any currency, so that you can end up with some amount greater than A of that currency.
+"""
+
+class Problem32(object):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def solution(self, arr):
+        pass
+
+if __name__ == "__main__":
+    pb32 = Problem32()
+    arr = [[], []]
+    pb32.solution(arr)
