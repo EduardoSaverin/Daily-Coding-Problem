db016961c0717b87fd679f47d074fa117b77addd
Sumit Chaudhary <EduardoSaverin@users.noreply.github.com>
2021-12-10 18:11:52 +0530

    Time: 48 ms (31.54%), Space: 15 MB (24.87%) - LeetHub

--- domino-and-tromino-tiling/domino-and-tromino-tiling.py
+++ domino-and-tromino-tiling/domino-and-tromino-tiling.py
@@ -1,7 +1,31 @@
 class Solution:
+    def __init__(self):
+        self.MOD = 1000000007
+        
     def numTilings(self, n: int) -> int:
-        dp = [1,2,5] + [0]*n
-        for index in range(3, n):
-            dp[index] = (dp[index-1]*2 + dp[index-3]) % 1000000007
-        return dp[n-1]
-# based on this explanation : https://leetcode.com/problems/domino-and-tromino-tiling/discuss/1620809/PythonJAVACC%2B%2B-DP-oror-Visualized-Explanation-oror-100-Faster-oror-O(N)
+        d = {}
+        d[1] = 1 # For n=1 Just one Solution
+        d[2] = 2 # For n=2 2 ways exists
+        d[3] = 5 # For n=3 5 ways as given in question example 1
+        d[1.5] = 1 # We know this from Tromino tile
+        result = self.countTiles(n, d)
+        return result
+    
+    def countTiles(self, n:int, d):
+        if n in d:
+            return d[n]
+        if n < 1:
+            return 0
+        count = 0
+        if n%1 == 0:
+            # This case means no half point tile coming out and it is full rectangle till now
+            # Not Three Cases Are there
+            # Case 1 : One Vertical Domino
+            # Case 2 : 2 Horizontal Domino
+            # Case 3 : One Tromino, Here we can multiply by 2 since we can place this in two ways. Notice -1.5 because tromino will occupy 1.5 space
+            count = (self.countTiles(n-1, d) + self.countTiles(n-2, d) + 2*self.countTiles(n-1.5, d)) % self.MOD
+        else:
+            # Here we can apply either Case 1 or Case 3 (only one time because we have to fill 0.5 space also and that can be one in one way only) only
+            count = (self.countTiles(n-1, d) + self.countTiles(n-1.5, d)) % self.MOD
+        d[n] = count
+        return count
