30e8123285d405ab2274e4e20565fe20e9761073
Sumit Chaudhary <EduardoSaverin@users.noreply.github.com>
2021-12-24 08:09:55 +0530

    Added new faster solution

--- subsets/subsets.py
+++ subsets/subsets.py
@@ -13,3 +13,16 @@ class Solution:
                 recursion(nums, i+1, result[:])
         recursion(nums, 0, [])
         return set(tuple(row) for row in l)
+    
+    
+# Faster Solution
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        self.dfs(nums ,[], result)
+        return result
+    
+    def dfs(self, nums, path, result):
+        result.append(path[:])
+        for index in range(len(nums)):
+            self.dfs(nums[index+1:], path + [nums[index]], result)
