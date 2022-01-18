aaf18ba5a39ea51932a81319d3b94abb82e89f40
Sumit Chaudhary <EduardoSaverin@users.noreply.github.com>
2022-01-18 08:17:33 +0530

    Time: 302 ms (8.32%), Space: 14.5 MB (61.08%) - LeetHub

--- 605-can-place-flowers/605-can-place-flowers.py
+++ 605-can-place-flowers/605-can-place-flowers.py
@@ -1,31 +1,8 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        self.flowerbed = flowerbed
-        for index in range(len(flowerbed)):
-            if n == 0 :
-                return True
-            if self.flowerbed[index] == 0 and self.isValid(index):
-                self.flowerbed[index] = 1
+        for index, val in enumerate(flowerbed):
+            if not val and (index == 0 or flowerbed[index-1] == 0) and (index == len(flowerbed)-1 or flowerbed[index+1] == 0):
                 n -= 1
-        return True if not n else False
-    
-    def isValid(self, index):
-        length = len(self.flowerbed)
-        if (index-1) >= 0 and (index+1) < length:
-            if self.flowerbed[index-1] != 1 and self.flowerbed[index+1]!=1:
-                return True
-            else:
-                return False
-        elif (index-1) >= 0:
-            if self.flowerbed[index-1] != 1:
-                return True
-            else:
-                return False
-        elif (index+1) < length:
-            if self.flowerbed[index+1]!=1:
-                return True
-            else:
-                return False
-        elif index == (length-1):
-            return True
+                flowerbed[index] = 1
+        return n <= 0
                     
