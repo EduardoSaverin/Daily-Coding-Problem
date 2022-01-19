81e3975ef7f412e2d5f0063b6f7942cde12abefb
Sumit Chaudhary <EduardoSaverin@users.noreply.github.com>
2022-01-19 08:45:39 +0530

    Time: 71 ms (31.04%), Space: 17.3 MB (56.58%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.py
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.py
@@ -5,7 +5,27 @@
 #         self.next = None
 from collections import defaultdict
 class Solution:
+    # Follow Up
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None or head.next is None:
+            return None
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                break
+        if slow != fast:
+            return None
+        slow = head
+        while slow != fast:
+            slow = slow.next
+            fast = fast.next
+        return slow
+                
+    
+    def detectCycleOld(self, head: Optional[ListNode]) -> Optional[ListNode]:
         s = set()
         ptr = head
         while ptr and ptr not in s:
