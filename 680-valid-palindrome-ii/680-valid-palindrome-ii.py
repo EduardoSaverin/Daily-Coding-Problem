3e44c2efaa70ec57104d669b9321e48e56cb4fc4
Sumit Chaudhary <EduardoSaverin@users.noreply.github.com>
2022-04-02 07:47:59 +0530

    Time: 336 ms (13.91%), Space: 14.5 MB (91.68%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.py
@@ -2,18 +2,16 @@ class Solution:
     def validPalindrome(self, s: str) -> bool:
         left = 0
         right = len(s) - 1
+        count = 1 # Number of deletes
+        return self.check_palindrome(s, left, right, count)
+        
+    def check_palindrome(self, string, left, right, count):
         while left < right:
-            if s[left] != s[right] :
-                return self.check_palindrome(s, left, right-1) or self.check_palindrome(s, left+1, right)
+            if string[left] != string[right]:
+                if count <= 0 :
+                    return False
+                return self.check_palindrome(string, left+1, right, count-1) or self.check_palindrome(string, left, right-1, count-1)
             left += 1
             right -= 1
         return True
-        
-    def check_palindrome(self, s, left, right):
-            while left < right:
-                if s[left] != s[right]:
-                    return False
-                left += 1
-                right -= 1
-            return True
         
