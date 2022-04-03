d1e29c20351030b4658272a51fe463fdaa6d86a9
Sumit Chaudhary <EduardoSaverin@users.noreply.github.com>
2022-04-03 09:26:56 +0530

    Time: 3019 ms (20.49%), Space: 13.9 MB (72.13%) - LeetHub

---
+++ 1850-minimum-adjacent-swaps-to-reach-the-kth-smallest-number/1850-minimum-adjacent-swaps-to-reach-the-kth-smallest-number.py
@@ -0,0 +1,34 @@
+class Solution:
+    def getMinSwaps(self, num: str, k: int) -> int:
+        list_of_num = list(num)
+        for index in range(k):
+            self.nextPermutation(list_of_num)
+        count = 0
+        num = list(num)
+        length = len(num)
+        # Simply Compairing Number
+        for i in range(length):
+            j = i
+            while j < length and list_of_num[i] != num[j]:
+                j += 1
+            count += j - i
+            num[i:j+1] = [num[j]] + num[i:j]
+        return count
+    
+    # Taken From Problem 31
+    def nextPermutation(self, nums: List[int]) -> None:
+        i = j = len(nums)-1
+        while i > 0 and nums[i-1] >= nums[i]:
+            i -= 1
+        if i == 0:
+            nums.reverse()
+            return
+        while nums[j] <= nums[i-1]:
+            j -= 1
+        nums[i-1] , nums[j] = nums[j], nums[i-1]
+        left = i
+        right = len(nums)-1
+        while left < right:
+            nums[left], nums[right] = nums[right] , nums[left]
+            left += 1
+            right -= 1
