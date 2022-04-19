3696fb589bd1b624f8983a219b46701d0ccb4658
Sumit Chaudhary <EduardoSaverin@users.noreply.github.com>
2022-04-19 09:15:02 +0530

    Time: 77 ms (81.92%), Space: 14.3 MB (28.16%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.py
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.py
@@ -6,25 +6,35 @@
 #         self.right = right
 class Solution:
     def recoverTree(self, root: Optional[TreeNode]) -> None:
-        """
-        Do not return anything, modify root in-place instead.
-        """
-        self.first = None
-        self.second = None
-        self.prev = TreeNode(-float("inf"))
-        self.traverse(root)
-        self.first.val, self.second.val = self.second.val, self.first.val
-    
-    def traverse(self, root):
-        if not root:
-            return
-        self.traverse(root.left)
-        if self.first is None and self.prev.val >= root.val:
-            self.first = self.prev
-        if self.first is not None and self.prev.val >= root.val:
-            self.second = root
-        self.prev = root
-        self.traverse(root.right)
+        # Morris Traversal
+        # If you get confused , especially with condition at Line 22, read
+        # https://stackoverflow.com/questions/5502916/explain-morris-inorder-tree-traversal-without-using-stacks-or-recursion
+        current, arr = root, []
+        prev = TreeNode(-float("inf"))
+        while current:
+            if current.left is None:
+                if prev.val > current.val:
+                    arr += [prev, current]
+                prev = current
+                current = current.right
+            else:
+                pre = current.left
+                while pre.right and pre.right != current:
+                    pre = pre.right
+                if pre.right is None:
+                    pre.right = current
+                    current = current.left
+                else:
+                    pre.right = None
+                    if prev.val > current.val:
+                        arr += [prev, current]
+                    prev = current
+                    current = current.right
+        arr[0].val, arr[-1].val = arr[-1].val, arr[0].val
+                        
+                    
+                
+                        
             
         
         
