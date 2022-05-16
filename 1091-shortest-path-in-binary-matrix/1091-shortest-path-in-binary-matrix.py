7e6c36b1892a4863abf4de9302386b5c4a61a956
Sumit Chaudhary <EduardoSaverin@users.noreply.github.com>
2022-05-16 07:26:53 +0530

    Time: 826 ms (51.91%), Space: 15.5 MB (26.25%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -6,17 +6,16 @@ class Solution:
             return -1
         self.visited = set()
         q = []
-        distance = float('inf')
         q.append([0,0,1]) # index 0,0 with distance 1
         while q:
             row, col , dis = q.pop(0)
             if row == self.rows-1 and col == self.cols - 1:
-                distance = min(distance, dis)
+                return dis
             for x,y in [(0,1), (-1,1), (-1,0), (-1,-1), (0,-1), (1,-1), (1,0), (1,1)]:
                 if self.isValid(row+x, col+y) and grid[row+x][col+y] == 0 :
                     self.visited.add((row+x, col+y))
                     q.append([row+x, col+y, dis+1])
-        return -1 if distance == float("inf") else distance
+        return -1
     def isValid(self, row, col):
         if 0 <= row < self.rows and 0 <= col < self.cols and (row, col) not in self.visited:
             return True
