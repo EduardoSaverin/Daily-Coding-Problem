79fc7dd5f2803ca88fa2b1676637248b259d2119
Sumit Chaudhary <EduardoSaverin@users.noreply.github.com>
2022-05-17 07:20:08 +0530

    Time: 1046 ms (14.94%), Space: 24 MB (68.23%) - LeetHub

--- 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.py
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.py
@@ -7,23 +7,11 @@
 
 class Solution:
     def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
-        self.target = target
-        node1 = self.getNode(original)
-        node2 = self.getNode(cloned)
-        if self.isSame(node1, node2):
-            return node2
-        return target
-    
-    def getNode(self, root):
-        if root is None:
-            return
-        if root.val == self.target.val:
-            return root
-        return self.getNode(root.left) or self.getNode(root.right)
-        
-    def isSame(self, root1: TreeNode, root2: TreeNode) -> bool:
-        if root1 is None and root2 is None:
-            return True
-        if root1 is None or root2 is None:
-            return False
-        return root1.val == root2.val and self.isSame(root1.left, root2.left) and self.isSame(root1.right, root2.right)
+        def recursion(root1, root2):
+            if root1:
+                recursion(root1.left, root2.left)
+                if root1 is target:
+                    self.ans = root2
+                recursion(root1.right, root2.right)
+        recursion(original, cloned)
+        return self.ans
