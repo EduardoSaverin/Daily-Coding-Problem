bd7f76424224a0bdf14f52e4b4559e4c6a005f2e
Sumit Chaudhary <EduardoSaverin@users.noreply.github.com>
2022-05-21 10:08:10 +0530

    Time: 2056 ms (43.44%), Space: 44.5 MB (5.16%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,7 +1,15 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [0] + [float("inf")]*amount
-        for coin in coins:
-            for a in range(coin, amount+1):
-                dp[a] = min(dp[a], dp[a-coin]+1)
-        return -1 if dp[-1] == float("inf") else dp[-1]               
+        dp = {}
+        dp[0] = 0
+        def recursion(amount):
+            if amount in dp:
+                return dp[amount]
+            dp[amount] = float("inf")
+            for coin in coins:
+                if amount - coin >= 0 :
+                    dp[amount] = min(dp[amount], 1+recursion(amount-coin))
+            return dp[amount]
+        recursion(amount)
+        return -1 if dp[amount] == float("inf") else dp[amount]
+            
