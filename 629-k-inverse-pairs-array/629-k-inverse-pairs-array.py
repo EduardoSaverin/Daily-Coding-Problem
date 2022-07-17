850c3db3b797d104e78b60991d81cc6317d1727b
Sumit Chaudhary <EduardoSaverin@users.noreply.github.com>
2022-07-17 17:37:54 +0530

    Time: 490 ms (87.50%), Space: 52.2 MB (63.75%) - LeetHub

---
+++ 629-k-inverse-pairs-array/629-k-inverse-pairs-array.py
@@ -0,0 +1,19 @@
+class Solution:
+    def kInversePairs(self, n: int, k: int) -> int:
+        #.
+        MOD = 10**9+7
+        dp = [[0]*(k+1) for _ in range(n+1)]
+        dp[0][0] = 1
+        for i in range(1, n+1):
+            cumsum = 0
+            for j in range(k+1):
+                if j == 0:
+                    dp[i][j] = 1
+                    cumsum += 1
+                else:
+                    cumsum += dp[i-1][j]
+                    if j-i >= 0:
+                        cumsum -= dp[i-1][j-i]
+                    dp[i][j] = cumsum % MOD
+        return dp[n][k]
+            
