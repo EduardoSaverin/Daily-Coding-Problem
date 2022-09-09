41783ab2ac11f889af58399d388f0a84bfc20ee2
Sumit Chaudhary <EduardoSaverin@users.noreply.github.com>
2022-09-09 19:31:15 +0530

    Time: 4921 ms (7.35%), Space: 67.2 MB (63.30%) - LeetHub

---
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.py
@@ -0,0 +1,12 @@
+from functools import cmp_to_key
+class Solution:
+    def numberOfWeakCharacters(self, properties: List[List[int]]) -> int:
+        properties.sort(key=lambda x: (-x[0],x[1]))
+        count = 0
+        curr_max = 0
+        for _, d in properties:
+            if d < curr_max:
+                count += 1
+            else:
+                curr_max = d
+        return count
